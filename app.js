c59e43fb45bc07e4da94e1d1b0f7c8ce5e636736
jorge-mauricio <web@jorgemauricio.com>
2021-07-10 20:00:15 -0300

    Basic graphql setup.

--- app.js
+++ app.js
@@ -1,8 +1,62 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+//const graphqlHttp = require('express-graphql');
+const graphqlHTTP = require('express-graphql').graphqlHTTP;
+const { buildSchema } = require('graphql');
+
 
 const app = express();
 
 app.use(bodyParser.json());
+
+
+//GraphQL middleware.
+app.use('/graphql', 
+    graphqlHTTP({
+        //Schema.
+        schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }`),
+        //Resolver (need to have same names).
+        rootValue: {
+            //Resolver for reading array of strings.
+            events: () => {
+                return ['Romantic cooking', 'Sailing', 'All night coding']; //debug test
+            },
+            createEvent: (args) => {
+                const eventName = args.name; //same name as the argument defined in the mutation type
+                return eventName;
+            }
+        },
+        //User interface tool
+        graphiql: true
+        /*
+        Debug:
+        query {
+            events
+        }
+
+        */
+    })
+);
+
+
+//Testing server.
+/*
+app.get('/', (reg, res, next) => {
+    res.send('Server working.')
+});
+*/
+
 
 app.listen(3000);
